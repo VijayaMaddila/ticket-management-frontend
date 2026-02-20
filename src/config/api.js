b3782c8d0ae040c0e196da019b580517b77a7ab8
VijayaMaddila <saivijjivijji@gmail.com>
2026-02-20 09:29:50 +0530

    refactor:updated base url

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 
 // Base URL for backend
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL||"http://localhost:8080";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
   
 
